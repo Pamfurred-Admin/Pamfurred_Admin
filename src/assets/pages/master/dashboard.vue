4a2928fd33af3fa6be8c85ba6e3b4c6d3098999f
gonzagaaillen <gonzagaaillen@gmail.com>
2024-09-30 18:10:23 +0800

    Router Setup

---
+++ src/assets/pages/master/dashboard.vue
@@ -0,0 +1,14 @@
+/* eslint-disable vue/multi-word-component-names */
+<template>
+<div>this is my Dashboard</div>
+</template>
+
+<script>
+export default{
+    name: 'DashboardPage'
+};
+</script>
+
+<style>
+
+</style>
