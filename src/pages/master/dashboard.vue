00ea5c9907427bef04811410555caad131fac40d
gonzagaaillen <gonzagaaillen@gmail.com>
2024-11-05 17:47:24 +0800

    dashboard adjusted

--- src/pages/master/dashboard.vue
+++ src/pages/master/dashboard.vue
@@ -7,10 +7,10 @@
       <HeadeR />
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-1 p-4">
         <router-link to="/serviceprovider" class="bg-white border border-gray-200 rounded-lg p-4 shadow hover:shadow-lg transition-transform transform hover:scale-105">
-          <totalServiceprovider :totalServiceprovider="19" />
+          <totalServiceprovider/>
         </router-link>
         <router-link to="/petowners" class="bg-white border border-gray-200 rounded-lg p-4 shadow hover:shadow-lg transition-transform transform hover:scale-105">
-          <totalPetowners :totalPetowners="20" />
+          <totalPetowners/>
         </router-link>
         <div class="bg-white border border-gray-200 rounded-lg p-4 shadow hover:shadow-lg transition-transform transform hover:scale-105">
           <totalAppointments :totalAppointments="8291" />
